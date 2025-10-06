066575f3eb26d614867fc0198a0fbef018ee9589
vimalsahu07 <bloger24x7@gmail.com>
2025-10-06 14:37:21 +0530

    First

--- office/src/Navbar/Navbar.jsx
+++ office/src/Navbar/Navbar.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
+
+    <>
     <div>
-      
+    <div>
+      <h1 className='text-4xl font-bold'>Gym <span className='text-green-500'>Wala</span></h1>
+      </div>  
+      <div>
+        
+      </div>
     </div>
+    </>
   )
 }
 
